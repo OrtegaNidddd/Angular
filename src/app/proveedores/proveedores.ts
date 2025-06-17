4b7c8b1934f46cf6738898904d988bbcae14a837
Nick Ortega <orteganid@gmail.com>
2025-06-17 17:06:38 -0500

    Login finalizado con la ruta al home. Se empieza a realizar la sección de proveedores

---
+++ src/app/proveedores/proveedores.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proveedores',
+  imports: [],
+  templateUrl: './proveedores.html',
+  styles: ``
+})
+export class Proveedores {
+
+}
